cdef7beaa6f848588ba731190f051b24d367ec68
Valentino Giunchetti <valentino.giunchetti@ecocerved.it>
2025-03-26 09:49:24 +0100

    #10003 test string e aggiornato .gitignore
    # Conflicts:
    #	TestLib/TestLib/Class1.cs

--- TestLib/TestLib/Class1.cs
+++ TestLib/TestLib/Class1.cs
@@ -2,7 +2,8 @@
 {
     public class Class1
     {
-        string primaStringa = "Ciao Mappamondo!";
+        string primaStringa = "Ciao Amico del Mappamondo!";
+        string stringaContraria = "Ciao Nemico del Mondo!";
 
         string secondaStringa = "Nuovo branch feature, nome: 10002-branch-test-rename";
         string terzaStringa = "Nuovo branch feature, nome: 10003-cambio-nome";
