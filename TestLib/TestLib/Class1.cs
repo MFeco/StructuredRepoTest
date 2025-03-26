a15ab6d86be875d7272fca493a2f51986fd0bf33
Matteo Fabris <matteo.fabris@ecocerved.it>
2025-03-26 09:46:13 +0100

    #10003 aggiunta traccia del cambio nome

--- TestLib/TestLib/Class1.cs
+++ TestLib/TestLib/Class1.cs
@@ -5,5 +5,6 @@
         string primaStringa = "Ciao Mappamondo!";
 
         string secondaStringa = "Nuovo branch feature, nome: 10002-branch-test-rename";
+        string terzaStringa = "Nuovo branch feature, nome: 10003-cambio-nome";
     }
 }
